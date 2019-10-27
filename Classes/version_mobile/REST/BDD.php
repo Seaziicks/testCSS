eca73de0af0616e00f3adcd35cb49e6a24eb6f02
Esteban <esteban.baichoo@gmail.com>
2019-10-27 10:56:57 +0100

    Changing BDD.php everywhere, because not ignored in the hook ...

--- Classes/version_mobile/REST/BDD.php
+++ Classes/version_mobile/REST/BDD.php
@@ -1,11 +1,16 @@
-<?php
-// Sous WAMP (Windows)
+<?php
+
+// Sous WAMP (Windows)
+
 try
 {
-$bdd = new PDO('mysql:host=localhost;dbname=modifications(zone tampon);charset=utf8', 'root', '');
-}
+    $bdd = new PDO('mysql:host=localhost;dbname=u418341279_rp;charset=utf8', 'u418341279_root', 'banane00002');
+}
+
 catch(Exception $e)
 {
-	// En cas d'erreur, on affiche un message et on arrête tout	die('Erreur : '.$e->getMessage());
-}
+    // En cas d'erreur, on affiche un message et on arrête tout
+    die('Erreur : '.$e->getMessage());
+}
+
 ?>
