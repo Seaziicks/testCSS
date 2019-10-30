abe983ce89ce5aa12376ca4d7f5d9dd73940e858
Esteban <esteban.baichoo@gmail.com>
2019-10-30 15:47:48 +0100

    Continuing to build the environment for AddCompetenceEffect. Not far from first functional display. Still needing to complete the "AddCopetence", "AddEffect" buttons, and the from for managing existing effect.

---
+++ Classes/version_mobile/REST/effectAddCompetenceEffect.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+$competenceEffectsInfos = $bdd->query('SELECT ce.*, c.Niveau 
+                                        FROM competenceeffect ce, competence c
+                                        WHERE ce.idCompetenceEffect = '.$_GET['id'].'
+                                        AND ce.idCompetence = c.Id_Competence');
+
+$réponse=array();
+while($effect=$competenceEffectsInfos->fetch(PDO::FETCH_ASSOC)){
+
+    array_push($réponse,$effect);
+}
+
+function EnJson($arr , $format = 0){
+    header('Content-type: application/json;charset=utf-8'); //Setting the page Content-type
+    if($format){
+        return json_encode($arr,448);
+    }else{
+        return json_encode($arr);
+    }
+}
---
+++ Classes/version_mobile/REST/effectAddCompetenceEffectRest.php
@@ -0,0 +1,43 @@
+<?php
+
+// On enregistre notre autoload.
+function chargerClasse($classname)
+{
+    require 'Poo/'.$classname.'.php';
+}
+
+spl_autoload_register('chargerClasse');
+
+session_start(); // On appelle session_start() APRÈS avoir enregistré l'autoload.
+
+/// Librairies éventuelles (pour la connexion à la BDD, etc.)
+include('BDD.php');
+
+/// Paramétrage de l'entête HTTP (pour la réponse au Client)
+header("Content-Type:application/json");
+
+/// Identification du type de méthode HTTP envoyée par le client
+$http_method = $_SERVER['REQUEST_METHOD'];
+switch ($http_method){
+    /// Cas de la méthode GET
+    case "GET" :
+        include('effectAddCompetenceEffect.php');
+        $matchingData=$réponse;
+
+        /// Envoi de la réponse au Client
+        deliver_response(200, "Votre message", $matchingData);
+        break;
+}
+/// Envoi de la réponse au Client
+function deliver_response($status, $status_message, $data){
+    /// Paramétrage de l'entête HTTP, suite
+    header("HTTP/1.1 $status $status_message");
+    /// Paramétrage de la réponse retournée
+    $response['status'] = $status;
+    $response['status_message'] = $status_message;
+    $response['data'] = $data;
+    /// Mapping de la réponse au format JSON
+    $json_response = json_encode($data);
+
+    echo $json_response;
+}
