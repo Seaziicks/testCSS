e0caa8bd90a9b4d3440881b3d662e90a17321c12
Esteban <esteban.baichoo@gmail.com>
2019-11-03 12:47:52 +0100

    Trying to configure add and deletion to Competence. Added the competencelearned table in data base. We'll see if 'PUT' works !

---
+++ Classes/version_mobile/REST/addCompetence.php
@@ -0,0 +1,101 @@
+<?php
+// On enregistre notre autoload.
+function chargerClasse($classname)
+{
+    require 'Poo/' . $classname . '.php';
+}
+
+spl_autoload_register('chargerClasse');
+
+session_start(); // On appelle session_start() APRÈS avoir enregistré l'autoload.
+
+include('BDD.php');
+
+$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$http_method = $_SERVER['REQUEST_METHOD'];
+
+switch ($http_method) {
+    case "PUT" :
+        try {
+            $sql;
+            $competence = (array) json_decode($_GET['Competence']);
+            foreach($competence as $key => $value) {
+                if(empty($value) || is_null($value)){
+                    $competence[$key] = 'NULL';
+                } else if (is_string($value)) {
+                    $competence[$key] = '\''.$value.'\'';
+                }
+                if($key == 'pourcentage') {
+                    $competence[$key] = $competence[$key] == 'NULL' ? '0' : '1';
+                }
+            }
+
+            $sql = "INSERT INTO competence (Libellé, Niveau, Description1)
+            VALUES (" . $competence['competenceName'] . ", 1, 'Description de " . $competence['competenceName'] . "')";
+            // use exec() because no results are returned
+            $bdd->exec($sql);
+
+            $lastInsertID = $bdd->lastInsertId();
+
+            $sql = "INSERT INTO learnedcompetence (idPersonnage, idCompetence, niveau)
+            VALUES (" . $competence['idPersonnage'] . ", " . $lastInsertID . ", 1)";
+            // use exec() because no results are returned
+            $bdd->exec($sql);
+
+            $policy = [
+                [
+                    'Libellé' => $competence['competenceName'],
+                    'Niveau' => 1,
+                    'Description' => 'Description de ' . $competence['competenceName'] .'',
+                ],[
+                    'idPersonnage' => $competence['idPersonnage'],
+                    'idCompetence' => $lastInsertID,
+                    'niveau' => 1,
+                ]
+            ];
+
+            $bdd = null;
+            http_response_code(201);
+            deliver_responseRest(201, "New competence effect created successfully", $policy);
+        } catch
+        (PDOException $e) {
+            echo  $sql . "<br>" . $e->getMessage();
+        }
+        break;
+    case "DELETE" :
+        try {
+            echo 'On est dans le DELETE ! <br/><br/>';
+            $competence = (array)json_decode($_GET['Competence']);
+
+            $sql = "DELETE FROM learnedcompetence
+                WHERE idCompetence = " . $competence['idCompetence'] . "
+                AND idPersonnage = " . $competence['idPersonnage'] . "";
+            $bdd->exec($sql);
+
+            $sql = "DELETE FROM competence
+                WHERE Id_Competence = " . $competence['idCompetence'] . "";
+            $bdd->exec($sql);
+            $bdd = null;
+            http_response_code(200);
+            deliver_responseRest(200, "EffectCompetence modified", NULL);
+        } catch
+        (PDOException $e) {
+            echo  $sql . "<br>" . $e->getMessage();
+        }
+        break;
+}
+
+/// Envoi de la réponse au Client
+function deliver_responseRest($status, $status_message, $data)
+{
+    /// Paramétrage de l'entête HTTP, suite
+    // header("HTTP/1.1 $status $status_message");
+/// Paramétrage de la réponse retournée
+    $response['status'] = $status;
+    $response['status_message'] = $status_message;
+    $response['data'] = $data;
+/// Mapping de la réponse au format JSON
+    $json_response = json_encode($response);
+    echo $json_response;
+}
--- Classes/version_mobile/REST/competencesAddCompetenceEffect.php
+++ Classes/version_mobile/REST/competencesAddCompetenceEffect.php
@@ -7,11 +7,21 @@ $competencesInfos = $bdd->query('SELECT DISTINCT c.Libellé, c.Id_Competence, c.
 								WHERE ID_Personnage = ' . $_GET['id'] . ' 
 								AND c.id_competence in (Colonne1,Colonne2,Colonne3,Colonne4,Colonne5,
 								Colonne6,Colonne7,Colonne8,Colonne9)
-								ORDER BY Spécialité, Rang DESC');
+								ORDER BY c.Libellé');
+
+$futurCompetencesInfos = $bdd->query('SELECT Libellé, Id_Competence, Image
+								FROM competence , learnedcompetence
+								WHERE idPersonnage = ' . $_GET['id'] . ' 
+								ORDER BY Libellé');
 
 $reponse=array();
 
 while($competence=$competencesInfos->fetch(PDO::FETCH_ASSOC)){
+
+    array_push($reponse,$competence);
+}
+
+while($competence=$futurCompetencesInfos->fetch(PDO::FETCH_ASSOC)){
 
     array_push($reponse,$competence);
 }
