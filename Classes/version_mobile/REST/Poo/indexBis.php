9f3e4e594abe2a8a52d20479701754c88b02ce42
Esteban Baichoo <esteban.baichoo@gmail.com>
2020-07-24 15:37:07 +0200

    Adding bootstrap, font, and working on new welcome page, and moreover, new style.

---
+++ Classes/version_mobile/REST/Poo/indexBis.php
@@ -0,0 +1,81 @@
+<html lang="fr">
+<head>
+    <link rel="stylesheet" href="css/menuBis.css" type="text/css" media="screen"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Quest Paradise</title>
+    <link rel="icon" href="images/icon.png"/>
+</head>
+<body>
+
+    <nav class="navbar sticky-top navbar-expand-lg navbar-dark" style="background-color: #000000;">
+        <a class="navbar-brand" href="#">
+            <img src="images/icon.png" width="45" height="45" class="d-inline-block align-top" alt="">
+            Quest Paradise
+        </a>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Personnages
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown" data-animations="zoomIn" data-hover="dropdown">
+                        <a class="dropdown-item" href="#">Arbes</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Voleur</a>
+                        <a class="dropdown-item" href="#">Paladin</a>
+                        <a class="dropdown-item" href="#">Démoniste</a>
+                        <a class="dropdown-item" href="#">Nain</a>
+                        <a class="dropdown-item" href="#">Manipulateur de Sang</a>
+                        <a class="dropdown-item" href="#">Magmaticien</a>
+                        <a class="dropdown-item" href="#">Centaure</a>
+                        <a class="dropdown-item" href="#">Assassin</a>
+                        <a class="dropdown-item" href="#">Franklin</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Equipements
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown" data-animations="zoomIn" data-hover="dropdown">
+                        <a class="dropdown-item" href="#">Générateur</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Gestionnaire</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Auto-complétion</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Connexion</button>
+            </form>
+        </div>
+    </nav>
+    <main>
+        <div class="background-div"> </div>
+            <div class="bg-text">
+                <h1 style="font-family: GWTwoFont, serif; font-size: 120px">Quest Paradise</h1>
+                <p>Baichoo Esteban</p>
+            </div>
+
+    </main>
+    <!-- Footer -->
+    <footer class="page-footer font-small blue" style="background-color: #000000;">
+
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3" style="color:wheat">© 2020 Copyright:
+            Esteban Baichoo
+        </div>
+        <!-- Copyright -->
+
+    </footer>
+    <!-- Footer -->
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
