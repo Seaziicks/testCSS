3079cc1b71c968a05449f0022ce0e7acfba086e8
Esteban Baichoo <esteban.baichoo@gmail.com>
2020-07-29 14:25:30 +0200

    Cleaning.

--- Classes/version_mobile/REST/addCompetenceEffect.php
+++ Classes/version_mobile/REST/addCompetenceEffect.php
@@ -2,12 +2,12 @@
 // On enregistre notre autoload.
 function chargerClasse($classname)
 {
-    if (is_file('Poo/Poo/'.$classname.'.php'))
-        require 'Poo/Poo/'.$classname.'.php';
-    elseif (is_file('Poo/Poo/Manager/'.$classname.'.php'))
-        require 'Poo/Poo/Manager/'.$classname.'.php';
-    elseif (is_file('Poo/Poo/Classes/'.$classname.'.php'))
-        require 'Poo/Poo/Classes/'.$classname.'.php';
+    if (is_file('Poo/Poo/' . $classname . '.php'))
+        require 'Poo/Poo/' . $classname . '.php';
+    elseif (is_file('Poo/Poo/Manager/' . $classname . '.php'))
+        require 'Poo/Poo/Manager/' . $classname . '.php';
+    elseif (is_file('Poo/Poo/Classes/' . $classname . '.php'))
+        require 'Poo/Poo/Classes/' . $classname . '.php';
 }
 
 spl_autoload_register('chargerClasse');
@@ -19,24 +19,24 @@ include('BDD.php');
 $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $http_method = $_SERVER['REQUEST_METHOD'];
+$sql;
+$competenceEffect = (array)json_decode($_GET['EffectCompetence']);
+foreach ($competenceEffect as $key => $value) {
+    if (empty($value) || is_null($value)) {
+        $competenceEffect[$key] = 'NULL';
+    } else if (is_string($value)) {
+        $competenceEffect[$key] = '\'' . $value . '\'';
+    }
+    if ($key == 'pourcentage') {
+        $competenceEffect[$key] = $competenceEffect[$key] == 'NULL' ? '0' : '1';
+    }
+}
 
 switch ($http_method) {
     case "POST" :
-        try {
-            $sql;
-            $competenceEffect = (array) json_decode($_GET['EffectCompetence']);
-            foreach($competenceEffect as $key => $value) {
-                if(empty($value) || is_null($value)){
-                    $competenceEffect[$key] = 'NULL';
-                } else if (is_string($value)) {
-                    $competenceEffect[$key] = '\''.$value.'\'';
-                }
-                if($key == 'pourcentage') {
-                    $competenceEffect[$key] = $competenceEffect[$key] == 'NULL' ? '0' : '1';
-                }
-            }
 
-            if ($competenceEffect['idCompetenceEffect'] == -1) {
+        if ($competenceEffect['idCompetenceEffect'] == -1) {
+            try {
                 $sql = "INSERT INTO competenceeffect (effectOrder, idCompetence, actionType, effectType, niveauRequis, 
                               typeCalcul, calcul1A, calcul1B, calcul2A, calcul2B, amplitude, nombreAmplitude,
                               statistique1, statistique2, impact, pourcentage, numberOfUse, numberOfTurn, numberOfFight)
@@ -44,7 +44,7 @@ switch ($http_method) {
             " . $competenceEffect['actionType'] . ", " . $competenceEffect['effectType'] . ",
             " . $competenceEffect['niveauRequis'] . ", " . $competenceEffect['typeCalcul'] . ",
                     " . $competenceEffect['calcul1A'] . ", " . $competenceEffect['calcul1B'] . ",
-                    " . $competenceEffect['calcul2A']. ", " . $competenceEffect['calcul2B'] . ",
+                    " . $competenceEffect['calcul2A'] . ", " . $competenceEffect['calcul2B'] . ",
                     " . $competenceEffect['amplitude'] . ", " . $competenceEffect['nombreAmplitude'] . "
                     ," . $competenceEffect['statistique1'] . ", " . $competenceEffect['statistique2'] . ",
                     NULL, " . $competenceEffect['pourcentage'] . ",
@@ -78,7 +78,11 @@ switch ($http_method) {
                 $bdd = null;
                 http_response_code(201);
                 deliver_responseRest(201, "New competence effect created successfully", $policy);
-            } else {
+            } catch (PDOException $e) {
+                deliver_responseRest(400, "EffectCompetence modification error", $sql . "<br>" . $e->getMessage());
+            }
+        } else {
+            try {
                 $sql = "UPDATE competenceeffect 
                 SET effectOrder = " . $competenceEffect['effectOrder'] . ", idCompetence = " . $competenceEffect['idCompetence'] . ",
                 actionType = " . $competenceEffect['actionType'] . ", effectType = " . $competenceEffect['effectType'] . ",
@@ -93,22 +97,20 @@ switch ($http_method) {
                 WHERE idCompetenceEffect = " . $competenceEffect['idCompetenceEffect'];
 
 
-
                 $bdd->exec($sql);
-                $result = $bdd->query('SELECT * FROM competenceeffect WHERE idCompetenceEffect = . $competenceEffect[\'idCompetenceEffect\']');
+                $result = $bdd->query('SELECT * FROM competenceeffect WHERE idCompetenceEffect = ' . $competenceEffect['idCompetenceEffect'].'');
+                $fetchedResult = $result->fetch(PDO::FETCH_ASSOC);
                 $result->closeCursor();
                 $bdd = null;
                 http_response_code(200);
-                deliver_responseRest(200, "EffectCompetence modified", $result->fetch(PDO::FETCH_ASSOC));
+                deliver_responseRest(200, "EffectCompetence modified", $fetchedResult);
+            } catch (PDOException $e) {
+                deliver_responseRest(400, "EffectCompetence modification error", $sql . "<br>" . $e->getMessage());
             }
-        } catch
-        (PDOException $e) {
-            echo  $sql . "<br>" . $e->getMessage();
         }
         break;
     case "DELETE" :
         try {
-            $competenceEffect = (array)json_decode($_GET['EffectCompetence']);
             $sql = "DELETE FROM competenceeffect
                 WHERE idCompetenceEffect = " . $competenceEffect['idCompetenceEffect'] . "";
             $bdd->exec($sql);
@@ -117,7 +119,7 @@ switch ($http_method) {
             deliver_responseRest(200, "EffectCompetence modified", NULL);
         } catch
         (PDOException $e) {
-            echo  $sql . "<br>" . $e->getMessage();
+            echo $sql . "<br>" . $e->getMessage();
         }
         break;
 }
